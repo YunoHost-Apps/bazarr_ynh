c45366a890d53e4eaf67623ce4c9dc8caee0fc9d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-10-27 15:18:33 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,6 @@
 python_version="3.7"
 python_major_version=$(echo $python_version | cut -d. -f1)
 
-#REMOVEME? pkg_dependencies="libxml2-dev libxslt1-dev unrar-free ffmpeg libatlas-base-dev"
 pkg_dependencies+=" python${python_major_version}-libxml2 python${python_major_version}-lxml"
 pkg_dependencies+=" python${python_major_version}-dev python${python_major_version}-venv"
 
@@ -19,9 +18,9 @@ detect_and_read_radarr_and_sonarr_settings() {
 
 if ls /etc/yunohost/apps | grep -q "radarr"; then
         radarr_installed=True
-#REMOVEME?         radarr_port=$(ynh_app_setting_get --app radarr --key port)
-#REMOVEME?         radarr_path=$(ynh_app_setting_get --app radarr --key path)
-#REMOVEME?         radarr_api_key=$(ynh_app_setting_get --app radarr --key api_key)
+        radarr_port=$(ynh_app_setting_get --app radarr --key port)
+        radarr_path=$(ynh_app_setting_get --app radarr --key path)
+        radarr_api_key=$(ynh_app_setting_get --app radarr --key api_key)
 else
         radarr_installed=False
         radarr_port=""
@@ -31,9 +30,9 @@ fi
 
 if ls /etc/yunohost/apps | grep -q "sonarr"; then
         sonarr_installed=True
-#REMOVEME?         sonarr_port=$(ynh_app_setting_get --app sonarr --key port)
-#REMOVEME?         sonarr_path=$(ynh_app_setting_get --app sonarr --key path)
-#REMOVEME?         sonarr_api_key=$(ynh_app_setting_get --app sonarr --key api_key)
+        sonarr_port=$(ynh_app_setting_get --app sonarr --key port)
+        sonarr_path=$(ynh_app_setting_get --app sonarr --key path)
+        sonarr_api_key=$(ynh_app_setting_get --app sonarr --key api_key)
 else
         sonarr_installed=False
         sonarr_port=""
